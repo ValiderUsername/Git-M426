32e848ac1125d6b55941bbc3e02a1ceb37497435
TillLS <till@kauz.ch>
2022-11-08 10:16:33 +0100

    Ja

--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,16 +1,39 @@
-﻿namespace FooBarQix
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net.Http.Json;
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
-        public string Foo => "foo";
-
         public string Determine(int input)
         {
-            if(input == 3)
+            string result = "";
+            if (input % 3 == 0)
             {
-                return this.Foo;
+                result += "FOO";
             }
-            return input.ToString();
+            if (input % 5 == 0)
+            {
+                result += "BAR";
+            }
+            if (input % 7 == 0)
+            {
+                result += "QIX";
+            }
+            string Ausgabe = string.valueOf(input);
+            for (int j = 0; j < Ausgabe.Length(); j++)
+            {
+                char element = Ausgabe.charAt(j);
+                if (element == '3')
+                    result += "Foo";
+                if (element == '5')
+                    result += "Bar";
+                if (element == '7')
+                    result += "Qix";
+            }
+            return result.isEmpty() ? input : result;
         }
     }
 }
